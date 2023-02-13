e6fa6827bf145213c694a5d008796b8e0001ff06
Trevodorax <phvcgdx@gmail.com>
2023-02-13 02:23:18 -1100

    add CF check in random

--- randomNumber.asm
+++ randomNumber.asm
@@ -6,14 +6,16 @@ getRandomNumber:
     push rbp
     mov rbp, rsp
 
-    mov r8, 0
-    RDRAND r8
-    mov rax, r8
-    mov rbx, rdi
-    xor rdx, rdx
-    div rbx
-    mov r8, rdx
-    mov rax, r8
+    get_rand:
+        mov r8, 0
+        RDRAND r8
+        mov rax, r8
+        mov rbx, rdi
+        jnc get_rand
+        xor rdx, rdx
+        div rbx
+        mov r8, rdx
+        mov rax, r8
 
     mov rsp, rbp
     pop rbp
